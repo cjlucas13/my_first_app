6b01e2de5de2f4a42b0b61a997f28fcdc60cdf63
cjlucas13 <62869337+cjlucas13@users.noreply.github.com>
2020-10-02 16:41:38 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob
 from datetime import datetime
 
 Builder.load_file('design.kv')
@@ -9,6 +9,16 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]["password"] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password"
+
+
 
 class RootWidget(ScreenManager):
     pass
@@ -24,6 +34,22 @@ class SignUpScreen(Screen):
         
         with open("users.json", 'w') as file:
             json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+        print(available_feelings)
 
 class MainApp(App):
     def build(self):
